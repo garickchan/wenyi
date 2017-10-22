b39ffb2619221476f142a2c3818285e9eb303f39
chenxiaoqiang <357389356@qq.com>
2017-10-22 18:29:50 +0800

    xxoo

---
+++ docs/index.php
@@ -0,0 +1,257 @@
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="format-detection" content="telephone=no" />
+    <meta name="viewport" content="width=device-width, initial-scale=0.6, user-scalable=no">
+    <link rel="SHORTCUT ICON" href="wenyi.ico"/>
+    <title>闻祎互动&nbsp;|&nbsp;UI设计&nbsp;|&nbsp;网页设计</title>
+    <!-- Bootstrap -->
+    <link href="css/bootstrap.min.css" rel="stylesheet">
+    <link href="css/wenyi.css" rel="stylesheet">
+    <!-- IE兼容html5 -->
+    <script src="js/respond.min.js"></script>
+    <!-- IE兼容css3 -->
+    <script src="js/html5shiv.min.js"></script>
+</head>
+
+<body  data-spy="scroll" data-target=".navbar" data-offset="100">
+<!-- Static navbar -->
+<nav class="navbar navbar-default navbar-fixed-top">
+  <div class="navbar-header">
+    <a class="navbar-brand" href="#"><img src="images/logo.png"></a>
+    <button type="button" class="navbar-toggle" data-toggle="collapse" data-target="#navbar" aria-expanded="true" aria-controls="navbar">
+      <span class="sr-only">Toggle navigation</span>
+      <span class="icon-bar"></span>
+      <span class="icon-bar"></span>
+      <span class="icon-bar"></span>
+    </button>
+  </div>
+  <div class="phone visible-lg-inline-block">咨询热线：<span>185 8842 8630</span></div>
+  <div id="navbar" class="navbar-collapse collapse" aria-expanded="true">
+    <ul class="nav navbar-nav navbar-right">
+      <li class="active"><a href="#top">首页</a></li>
+      <li><a href="#service">服务范围</a></li>
+      <li><a href="#case">成功案例</a></li>
+      <li><a href="#Clients">合作客户</a></li>
+      <li><a href="#About">关于我们</a></li>
+    </ul>
+  </div><!--/.nav-collapse -->
+</nav>
+
+
+
+
+<!--banner开始-->
+<div class="bannerbox">
+	<div id="top"></div>
+	<div class="banner"></div>
+</div>
+<!--banner结束-->
+<div class="content_bigbox" id="content_bigbox">
+<!--service开始-->
+<div class="container" id="service_bg">
+	<div id="service"></div>
+	<div class="contengt_top"></div>
+    <div class="row">
+    	<div class="all_title" id="service_title"></div>
+        <div class="servicebox">
+        	<ul class="row">
+            	<li class="col-sm-4 col-xs-6">用户体验设计<span>UE DESIGN</span></li>
+                <li class="col-sm-4 col-xs-6">界面设计<span>UI DESIGN</span></li>
+                <li class="col-sm-4 col-xs-6">交互设计<span>UX DESIGN</span></li>
+                <li class="col-sm-4 col-xs-6">网站设计与开发<span>WEB DESIGN & DEV</span></li>
+                <li class="col-sm-8 col-xs-6">品牌形象设计<span>BRAND IDENTITYBRAND</span></li>
+            </ul>
+        </div>
+    </div>
+</div>
+<!--service结束-->
+
+<!--case开始-->
+<div class="container">
+  <div class="row">
+    <div id="case"></div>
+    <div class="all_title" id="case_title"></div>
+    <div class="casebox">
+    <ul class="case_click">
+        <li title="" class="col-lg-3 col-sm-4 col-xs-6" ><span>迪安特官网</span><img src="images/diante.png" /></li>
+          <li title="" class="col-lg-3 col-sm-4 col-xs-6" ><span>零距离</span><img src="images/lingjuli.png" /></li>
+          <li title="" class="col-lg-3 col-sm-4 col-xs-6"><span>电信</span><img src="images/dianxin.png" /></li>
+          <li title="" class="col-lg-3 col-sm-4 col-xs-6"><span>微护</span><img src="images/weilu.png" /></li>
+          <li title="" class="col-lg-3 col-sm-4 col-xs-6"><span>悟空买房</span><img src="images/wukong.png" /></li>
+          <li title="" class="col-lg-3 col-sm-4 col-xs-6"><span>博林天瑞</span><img src="images/bolin.png" /></li>
+          <li title="" class="col-lg-3 col-sm-4 col-xs-6"><span>智能体温计</span><img src="images/tiwenji.png" /></li>
+          <li title="" class="col-lg-3 col-sm-4 col-xs-6"><img src="images/more.png" /></li>
+      </ul>
+    </div>
+  </div>
+</div>
+<!--case结束-->
+
+<!--Clients开始-->
+<div class="container">
+    <div class="row">
+    	<div id="Clients"></div>
+    	<div class="all_title" id="Clients_title"></div>
+        <ul class="Clientsbox">
+            <li class="Clients1 col-sm-3 col-xs-4"></li>
+            <li class="Clients2 col-sm-3 col-xs-4"></li>
+            <li class="Clients3 col-sm-3 col-xs-4"></li>
+            <li class="Clients4 col-sm-3 col-xs-4"></li>
+            <li class="Clients5 col-sm-3 col-xs-4"></li>
+            <li class="Clients6 col-sm-3 col-xs-4"></li>
+            <li class="Clients7 col-sm-3 col-xs-4"></li>
+            <li class="Clients8 col-sm-3 col-xs-4"></li>
+        </ul>
+    </div>
+</div>
+<!--Clients结束-->
+
+<!--about开始-->
+<div class="container" id="About_bg">
+    <div class="row">
+    	<div class="contengt_bottom"></div>
+    	<div id="About"></div>
+    	<div class="all_title" id="About_title"></div>
+        <div class="About">
+            <p>闻祎科技成立于2014年，致力于互联网用户体验、界面设计、交互设计、网站建设。</p>
+    		<p>凭借出色的设计能力，在两年时间内，便建立了与20多家企业的合作关系，并得到客户的高度认可。</p>
+            <p>我们不信奉客户就是上帝，我们只信奉用户体验为王。</p>
+            <p>我们不但是这么说的，也是这么做的。</p>
+            <p>在与客户的合作过程中，除了美的讲究，更深入用户体验的细节，锱铢必较。</p>
+            <p>真诚期待与您合作：</p>
+            <b id="i_phone" class="col-xs-6 col-md-4">185 8842 8630</b>
+            <b id="i_qq" class="col-xs-6 col-md-4">357 389 356</b>
+        </div>
+    </div>
+</div>
+<!--about结束-->
+
+<!--footer开始-->
+<div class="footer">©闻祎科技, powered by wenyi design</div>
+<!--footer结束-->
+</div>
+<!--弹窗-->
+<div class="case_details_box"  id="container">
+    <div class="case_detailsbox" id="wrapper">
+        <div class="case_details">
+            <div class="details_right">
+                <div class="details_close"></div>
+                <ul>
+                    <li id="details_title">DKnight项目简介</li>
+                    <li id="details_und"></li>
+                    <li id="details_text">DKnight是湖南迪安特电子商务有限公司委托我们的官网设计开发项目。</li>
+                    <li id="details_text">迪安特的创始人从海外学成归来，在视觉上敢于大胆创新，采用了黑白色调，既符合了三款主打产品的气质，也突显了他们团队年轻、开放、向上的氛围。</li>
+                </ul>
+            </div>
+            <div class="details_left">
+                <img class="lazy" data-original="images/dknight.jpg" />
+            </div>
+        </div>
+        <div class="case_details">
+            <div class="details_right">
+                <div class="details_close"></div>
+                <ul>
+                    <li id="details_title">零距离项目简介</li>
+                    <li id="details_und"></li>
+                    <li id="details_text">零距离是一款女性私处训练软件。</li>
+                    <li id="details_text">女性私处训练在国内为一个比较私密的话题，故而在色彩选择上采用了神秘而性感的紫色，在图标的设计上也采用圆润柔和的圆形边框加圆角线条，使整体的设计更为女性接受。</li>
+                </ul>
+            </div>
+            <div class="details_left">
+                <img class="lazy" data-original="images/juli.jpg" />
+            </div>
+        </div>
+        <div class="case_details">
+            <div class="details_right">
+                <div class="details_close"></div>
+                <ul>
+                    <li id="details_title">Smartpal项目简介</li>
+                    <li id="details_und"></li>
+                    <li id="details_text">Smartpal是与大腕科技深度合作的项目。</li>
+                    <li id="details_text">这款APP是为配合大腕科技的智能手表而开发的应用。通过蓝牙连接实现智能手表与APP的通信，实时传输两端数据。除了多种运动模式选择外，还有社交、蓝牙电话、短信回复、心率测试等功能。</li>
+                    <li id="details_text">项目伊始，我们的团队便从项目立项、用户调研、竞品分析，一直跟进，直到给出交互设计及UI界面设计，可以说是一个非常成熟与成功的项目。
+</li>
+                </ul>
+            </div>
+            <div class="details_left">
+                <img class="lazy" data-original="images/smartpal.jpg" />
+            </div>
+        </div>
+        <div class="case_details">
+            <div class="details_right">
+                <div class="details_close"></div>
+                <ul>
+                    <li id="details_title">微护项目简介</li>
+                    <li id="details_und"></li>
+                    <li id="details_text">微护是与北京微护科技联手打造的护士交流考试平台，旨在为全国护士的考试、出国、交流创造一个网络平台。</li>
+                    <li id="details_text">在该项目的UI标准色方面，我们依据调研数据，确定了偏女生喜爱的蓝紫色系，配色也采用了明快活泼的颜色，以期更符合用户的审美与偏好。</li>
+                    
+                </ul>
+            </div>
+            <div class="details_left">
+                <img class="lazy" data-original="images/weihu.jpg" />
+            </div>
+        </div>
+        <div class="case_details">
+            <div class="details_right">
+                <div class="details_close"></div>
+                <ul>
+                    <li id="details_title">悟空买房项目简介</li>
+                    <li id="details_und"></li>
+                    <li id="details_text">悟空买房是一款购房意向者的线上交流平台。针对某个楼盘，有任何疑问都可以通过创建或加入群组来了解。</li>
+                    <li id="details_text">此次的UI设计为悟空买房的第一期需求，受到了客户的高度好评，同时达成了二期需求的继续合作。</li>
+                    
+                </ul>
+            </div>
+            <div class="details_left">
+                <img class="lazy" data-original="images/wuk.jpg" />
+            </div>
+        </div>
+        <div class="case_details">
+            <div class="details_right">
+                <div class="details_close"></div>
+                <ul>
+                    <li id="details_title">博林天瑞项目简介</li>
+                    <li id="details_und"></li>
+                    <li id="details_text">博林天瑞是博林集团在深圳南山区的一个地产项目。</li>
+                    <li id="details_text">此次博林天瑞项目的官网合作，旨在突出博林天瑞高端、时尚的气质。所以在网站的交互上，采用了一种非常新颖的方式；而在配色方面，则采用青色与金色搭配，突出其高端与尊贵。</li>
+                    
+                </ul>
+            </div>
+            <div class="details_left">
+                <img class="lazy" data-original="images/bolintianrui.jpg" />
+            </div>
+        </div>
+        <div class="case_details">
+            <div class="details_right">
+                <div class="details_close"></div>
+                <ul>
+                    <li id="details_title">智能体温计项目简介</li>
+                    <li id="details_und"></li>
+                    <li id="details_text">博林天瑞是博林集团在深圳南山区的一个地产项目。</li>
+                    <li id="details_text">此次博林天瑞项目的官网合作，旨在突出博林天瑞高端、时尚的气质。所以在网站的交互上，采用了一种非常新颖的方式；而在配色方面，则采用青色与金色搭配，突出其高端与尊贵。</li>                  
+                </ul>
+            </div>
+            <div class="details_left">
+                <img class="lazy" data-original="images/bolintianrui.jpg" />
+            </div>
+        </div>
+        <div id="details_switch" index=''>
+            <span class="details_switch_left">上一案例</span>
+            <span class="details_switch_right">下一案例</span>
+        </div>
+    </div>
+    <div id="case_details_bg"></div>
+</div>
+
+
+<script src="js/jquery-3.2.0.min.js"></script>
+<script src="js/jquery.lazyload.js"></script>
+<script src="js/iscroll.js"></script>
+<script src="js/bootstrap.min.js"></script>
+<script src="js/wenyi.js"></script>
+</body>
+</html>
---
+++ docs/js/wenyi.js
@@ -0,0 +1,111 @@
+﻿
+$(function load(){
+	//打开页面默认在页面顶部
+	window.parent.scrollTo(0, 0);
+
+//手机端点击隐藏nav
+$('.nav li').click(function(){
+	var _width = $(window).width(); 
+  if(_width < 768){
+	$('.navbar-collapse').attr('aria-expanded','false').removeClass('in').css('height','1px');
+	$('.navbar .navbar-toggle').removeClass("tog_bg")
+  }
+});
+
+//手机端菜单点击样式
+$('.navbar .navbar-toggle').click(function(){
+	$(this).toggleClass("tog_bg");
+});
+
+
+//service文字飞入动画
+$(document).scroll(function(){
+		var ser_obj = $(document).scrollTop();
+		var _width = $(window).width();
+		if(_width>1200){
+				if(ser_obj>900){
+					$('.servicebox').addClass('servicebox_anim');
+			  }
+	  
+	  }
+	  if(_width>768){
+				if(ser_obj>470){
+					$('.servicebox').addClass('servicebox_anim');
+			  }
+	  }
+	  if(_width < 768){
+	  	  if(ser_obj>220){
+					$('.servicebox').addClass('servicebox_anim');
+			  }
+	  }
+});
+
+
+
+//点击按钮和页面背景关闭弹窗
+$('.details_close,.nav ul li,#case_details_bg').click(function(){
+	$('html').removeClass('ac-gn-noscroll-long');
+	document.getElementById('case_title').scrollIntoView(true);
+	$('.case_details_box').hide();
+	
+})
+
+var img_fade = function() {
+	$("img.lazy").lazyload({
+		placeholder : "images/loading.gif",
+		effect: "show"
+	});
+}
+//点击方框展示弹框
+ var _index = 0;
+$(".case_click li").click(function(){
+  var _index = $(this).index();
+  var li_length = $(".case_click li").length;
+			if (_index!=li_length-1) {
+			  img_fade();
+				$('.case_detailsbox').scrollTop(0);
+				$('.details_left').scrollTop(0);
+				$('.case_details_box').show();
+				$('html').addClass('ac-gn-noscroll-long');	
+				$(".case_details").eq(_index).addClass("display").siblings().removeClass("display");
+			}else{
+				alert("该页面没有维护");
+			}
+});
+
+//点击下一个案例
+
+$(".details_switch_right").click(function(){
+	var case_has = $(".case_details").eq($(".case_details").length-1).hasClass("display");
+	if(!case_has){
+		img_fade();
+		$('.case_detailsbox').scrollTop(0);
+		$('.details_left').scrollTop(0);
+		$(".case_details.display").removeClass("display").next(".case_details").addClass("display");
+		}else {
+			alert("已经是最后一个案例");
+		}
+});
+//点击上一个案例
+$(".details_switch_left").click(function(){
+	var case_has = $(".case_details").eq(0).hasClass("display");
+	if(!case_has){
+		img_fade();
+		$('.case_detailsbox').scrollTop(0);
+		$('.details_left').scrollTop(0);
+		$(".case_details.display").removeClass("display").prev(".case_details").addClass("display");
+		}else {
+			alert("已经是第一个案例");
+		}
+});
+
+})
+var myscroll;
+		function loaded(){
+			setTimeout(function(){
+				myscroll=new iScroll("wrapper",{hScrollbar:false, vScrollbar:true, momentum:true});
+			}, 100 );
+		}
+		window.addEventListener("load",loaded,false);
+
+
